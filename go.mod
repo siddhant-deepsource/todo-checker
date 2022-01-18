0bffee08242d5cbe435d8fdffce465b4fe73ab5e
Siddhant N Trivedi <siddhant@deepsource.io>
2022-01-18 20:50:18 +0530

    init macro
    Signed-off-by: Siddhant N Trivedi <siddhant@deepsource.io>

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/siddhant-deepsource/macro-go
 
 go 1.17
+
+require github.com/siddhant-deepsource/godirwalkfork v0.0.0-20220118132429-d9dd16dc4cbe // indirect
---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+)
+
+var (
+	codePath    string = os.Getenv("CODE_PATH")
+	toolboxPath string = os.Getenv("TOOLBOX_PATH")
+	issues      []Issue
+)
+
+func main() {
+	analysisFiles, err := getAllFiles()
+	if err != nil {
+		log.Fatalln("Failed to read files to analyze.Exiting...")
+	}
+	fmt.Println(analysisFiles)
+
+	for _, path := range analysisFiles {
+		content, err := ioutil.ReadFile(path)
+		if err != nil {
+			log.Fatal(err)
+		}
+		lines := strings.Split(string(content), `\n`)
+		for lineNumber, line := range lines {
+			if strings.Contains(line, "TODO") {
+				createIssue(path, lineNumber, 0)
+			}
+		}
+	}
+	macroAnalysisResult := prepareResult()
+
+	if writeError := writeMacroResult(macroAnalysisResult); writeError != nil {
+		log.Fatalln("Error while writing results", writeError)
+	}
+}
